522b8a7cecdaf1608e150b13f6380cacc298e841
Aaryan Pashine <pashineaaryan@gmail.com>
2023-03-03 23:38:21 -0500

    cool stuff

--- sketch1/src/App.tsx
+++ sketch1/src/App.tsx
@@ -13,42 +13,64 @@ import "./styles.css";
 
 type Box = {
   id: number;
-  styles: any;
+  styles: Object;
   children: any[];
   active: Boolean;
 };
-// Ok so how am I going to do this
-// First create a box component,  it can be passed a signal that will take cake of its styling
-// It wil also have children in this signal, the children will also change and it will update in the box component
-// I should be able to update the signal whenever and the box will transform
 
-// what will the box component have?
-// I can have a set state method for the box, the state will contain the style and children
-// state would be an object signal that w
+const s = {
+  state1: {
+    index: 0,
+    styles: {
+      top: "400px",
+      left: "400px",
+      width: "500px",
+      height: "200px",
+    },
+  },
+  state2: {
+    index: 0,
+    styles: {
+      top: "400px",
+      left: "0px",
+      width: "800px",
+      height: "100px",
+    },
+  },
+  state3: {
+    index: 0,
+    styles: {
+      top: "600px",
+      left: "10px",
+      width: "400px",
+      height: "300px",
+      filter: "blur(4px)",
+    },
+  },
+  state4: {
+    index: 0,
+    styles: {
+      top: "250px",
+      left: "700px",
+      width: "400px",
+      height: "800px",
+    },
+  },
+};
 
-// what will we need to render a page
-// consider the data we will have
-// consider how many components it will take to construct the layout, each component the will have data as smaller components
-// eg: <p>{student()}</p>
-
-// what will we need to render a page
-// Data + Its design in html/css > Component + color, position, dimensions > How many components: active and inactivity
-
-// So we pass layout, a mega component an array of states
-// The layout component will run a <For> to make box components, if active
-// the box components will render its children based on data
-// the states can be changed by onclick events in components, they can change specific value of a specific box.
-
-// have template states.
-// make a state generator?
-
-const ProgramList = (programs: string[]) => {
-  return <For each={programs}>{(program) => <p>{program}</p>}</For>;
+const ProgramList = (programs: (string | Function)[]) => {
+  return (
+    <For each={programs}>
+      {(program) => <p>{typeof program === "string" ? program : program()}</p>}
+    </For>
+  );
 };
 
 const Menu = () => {
   return (
-    <div style="postion: fixed; width: 100px; height: 50px; background-color: orange;"></div>
+    <div style="padding: 10px; font-size: 20px">
+      this is another Component, that I can change using states
+    </div>
   );
 };
 
@@ -61,12 +83,22 @@ const state = createMutable<Box[]>([
       left: "200px",
       width: "100px",
       height: "100px",
-      transition: "200ms ease all",
+      color: "white",
+      transition: "300ms ease-in-out all",
     },
-    children: [ProgramList(["Graphic deisng", "illustration"]), Menu],
+    children: [ProgramList(["Design", "Illustration"])],
     active: true,
   },
 ]);
+
+function applyStates(index: number, styles: Object, children?: Function[]) {
+  for (const [key, value] of Object.entries(styles)) {
+    if (value === "DELETE") delete state[index].styles[key];
+    else state[index].styles[key] = value;
+  }
+
+  if (children) state[index].children = children;
+}
 
 function compileCss(styles: Object) {
   // TODO add a camel case to kebab case converter
@@ -84,14 +116,7 @@ const Box: Component = (props: any) => {
   });
   return (
     <>
-      <div
-        onClick={() => {
-          if (props.state.styles.left === "100px") {
-            props.state.styles.left = "600px";
-          } else props.state.styles.left = "100px";
-        }}
-        style={compiledStyles() + "background-color: red"}
-      >
+      <div style={compiledStyles() + "background-color: black"}>
         {props.children}
       </div>
     </>
@@ -101,16 +126,32 @@ const Box: Component = (props: any) => {
 createEffect(() => {
   console.log(state[0].styles);
 });
+const Buttons: Component = () => {
+  return (
+    <>
+      <button
+        onClick={() =>
+          applyStates(0, s.state1.styles, [
+            ProgramList(["Graphic Design, Illustration"]),
+          ])
+        }
+      >
+        State 1
+      </button>
+      <button onClick={() => applyStates(0, s.state2.styles, [Menu])}>
+        State 2
+      </button>
+      <button onClick={() => applyStates(0, s.state3.styles)}> State 3 </button>
+      <button onClick={() => applyStates(0, s.state4.styles)}> State 4 </button>
+    </>
+  );
+};
 
-function pleaseWork() {
-  if (state[0].styles.top === "100px") state[0].styles.top = "400px";
-  else state[0].styles.top = "100px";
-}
 const App: Component = () => {
   return (
     <div class="container">
+      <Buttons></Buttons>
       <Box state={state[0]}>{state[0].children[0]}</Box>
-      <button onClick={pleaseWork}>{state[0].name} see</button>
     </div>
   );
 };
