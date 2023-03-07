164f8b3a5429ba6db23f1fb8b66084c9ac45451d
ɹ̰ᴉ̰ɯⱯ̰ <95018580+PascalWager@users.noreply.github.com>
2023-03-07 00:24:13 -0500

    heal

--- sketch1/src/Back.tsx
+++ sketch1/src/Back.tsx
@@ -1,5 +1,7 @@
-import { For, Component, Setter, Accessor } from "solid-js";
+import { For, Component, Setter, Accessor, enableScheduling } from "solid-js";
 import { Layout } from "./Generator/Layout";
+import { randBias } from "./Generator/Utils";
+import { createQtGrid } from "./Generator/createQtGrid";
 import "./styles.css";
 
 const programs = [
@@ -10,24 +12,110 @@ const programs = [
   "EXPERIMENTAL ANIMATION",
   "DRAWING AND PAINTING",
 ];
-
-const Node: Component<{ left: string; top: string }> = (props) => {
+// order-radius:${Math.random() * 100}% ${Math.random() * 100}% ${
+//   Math.random() * 100
+// }% ${Math.random() * 100}%;
+const Node: Component<{
+  left: string;
+  top: string;
+  width: string;
+  height: string;
+  s: string;
+}> = (props) => {
   return (
     <div
-      style={`position: absolute; width: 20px; height: 20px; background-color: red; top: ${props.top}; left: ${props.left}`}
-    ></div>
+      class="temp"
+      style={`position: absolute;
+      width: ${props.width}; height: ${props.height}; 
+      background-color:${props.s}; top: ${props.top}; left: ${props.left}; 
+      `}
+    >
+      <span style="position:relative;" class="title">
+        Amir Nevis
+      </span>
+      <p style="position:relative" class="boxtext">
+        The thing that pisses me off the most is the degradation of the
+        intellectual role of the designer." This is what my friend tells me, as
+        we listen to each other’s anguished outpourings replete with VAT
+        numbers, freelancing and short-term contracts. And that made me wonder
+        what constitutes that role, whether it actually existed, how it vanished
+        and what replaced it. Trying to answer these questions, I’d like to
+        focus on graphic design as it is the field where I come from, and I
+        believe it represents a paradigmatic case within the so-called creative
+        industries. These days we hear a lot about the gig economy, the economy
+        of little jobs, of chores. Platforms like Uber, AirBnB or Foodora are
+        quickly becoming the direct intermediaries of any kind of service, a
+        seemingly unavoidable development since they are prone to
+        monopolization. Graphic design is no exception: online marketplaces like
+        Fiverr and TaskRabbit offer access to graphic designers able to create a
+        logo, a website, a poster.
+      </p>
+    </div>
   );
 };
-
+// border: 5px solid #131194;
 const nodeCollection: any[] = [];
-for (let i = 0; i < 10; i++) {
+const focus = {
+  x: Math.random() * 800 + 50,
+  y: Math.random() * 800 + 50,
+};
+const height: number = window.innerHeight;
+const width: number = window.innerWidth;
+
+const points = [...Array(25)].map(() => {
+  return {
+    x: randBias(0, width, focus.x, 0.4),
+    y: randBias(0, height, focus.y, 0.4),
+    width: 1,
+    height: 1,
+  };
+});
+//console.log(points);
+const grid = createQtGrid({
+  width,
+  height,
+  points,
+  gap: 1,
+  maxQtLevels: 4,
+});
+// console.log("this is grid:");
+// console.log(grid);
+// grid.areas.forEach((gridarea: any) => {
+//   //console.log(gridarea);
+//   nodeCollection.push(
+//     <Node
+//       left={gridarea.x + "px"}
+//       top={gridarea.y + "px"}
+//       width={gridarea.width + "px"}
+//       height={gridarea.height + "px"}
+//       s={`rgba(${Math.random() * 255},${Math.random() * 255},${
+//         (Math.random() * 255, 1)
+//       })`}
+//     ></Node>
+//   );
+// });
+// const colors = ["#f815fc", "#58f783", "#fe7530"];
+// const colors = ["#f815fc", "#58f783"];
+points.forEach((p: any) => {
+  console.log(p);
+  let choose = Math.random() * 3;
+  choose = Math.floor(choose);
   nodeCollection.push(
     <Node
-      left={Math.random() * 1200 + "px"}
-      top={Math.random() * 800 + "px"}
+      left={p.x + "px"}
+      top={p.y + "px"}
+      // width={randBias(0, 10, focus.x, 0.5) + "px"}
+      // height={randBias(0, 10, focus.x, 0.5) + "px"}
+      width={Math.random() * 400 + 100 + "px"}
+      height={Math.random() * 400 + 100 + "px"}
+      // s={"rg255 134 19)b("}
+      s={"rgb(255 255 0)"}
+      // s={`rgba(${Math.random() * 255},${Math.random() * 255},${
+      //   (Math.random() * 255, 1)
+      // })`}
     ></Node>
   );
-}
+});
 
 const Back: Component<{ setOpen: Setter<Boolean>; open: Accessor<Boolean> }> = (
   props
@@ -41,7 +129,8 @@ const Back: Component<{ setOpen: Setter<Boolean>; open: Accessor<Boolean> }> = (
         left: "0px",
         width: "100vw",
         height: "100vh",
-        backgroundColor: "rgb(10, 10, 10)",
+        backgroundColor: "rgb(239 239 239)",
+        overflowY: "scroll",
       },
       children: nodeCollection,
       active: true,
@@ -51,3 +140,4 @@ const Back: Component<{ setOpen: Setter<Boolean>; open: Accessor<Boolean> }> = (
 };
 
 export default Back;
+// backgroundColor: "#131194",
